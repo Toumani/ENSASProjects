7739821cbdfe0930ab7c877d04a65e44cddb83a4
SANAEENSA <sanaegardouh1996@gmail.com>
2018-05-28 15:33:41 +0200

    modif_inscription

--- Acceuil.php
+++ Acceuil.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8"/>
 	<title>acceuil page</title>
 </head>
 <body>
-<h3>Bienvenuuu nos visiteurs</h3>
+<h3>Bienvenuuu nos visiteur</h3>
+<h2> cette page sera modifier</h2>
 </body>
 </html>
--- Inscription.php
+++ Inscription.php
@@ -1,3 +1,29 @@
+<?php
+if(!empty($_POST['text1']))
+{
+// D'abord, je me connecte à la base de données.
+mysql_connect("localhost", "root", "");
+mysql_select_db("demo");
+
+// Je mets aussi certaines sécurités ici…
+$passe = mysql_real_escape_string(htmlspecialchars($_POST['number']));
+$passe2 = mysql_real_escape_string(htmlspecialchars($_POST['password']));
+if($passe == $passe2)
+{
+$pseudo = mysql_real_escape_string(htmlspecialchars($_POST['text1']));
+$email = mysql_real_escape_string(htmlspecialchars($_POST['email']));
+// Je vais crypter le mot de passe.
+$passe = sha1($passe);
+
+mysql_query("INSERT INTO validation VALUES('', '$pseudo', '$passe', '$email')");
+}
+ 
+else
+{
+echo 'Les deux mots de passe que vous avez rentrés ne correspondent pas…';
+}
+}
+?>
 
 <!DOCTYPE html>
 <html lang="zxx">
@@ -26,11 +52,11 @@
     <!-- //fonts -->
     <link rel="stylesheet" href="csss/font-awesome.css" type="text/css" media="all">
     <!-- //Font-Awesome-File-Links -->
-	
-	<!-- Google fonts -->
-	<link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+    
+    <!-- Google fonts -->
+    <link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
 
-	<!-- Google fonts -->
+    <!-- Google fonts -->
 
 </head>
 <!-- //Head -->
@@ -40,7 +66,7 @@
     <h1 class="title-agile text-center">Veuillez Inscrivez vous!!</h1>
     <div class="content-w3ls">
         <div class="content-bottom">
-			<h2>Register Here <i class="fa fa-hand-o-down" aria-hidden="true"></i></h2>
+            <h2>Register Here <i class="fa fa-hand-o-down" aria-hidden="true"></i></h2>
             <form action="#" method="post">
                 <div class="field-group">
                     <span class="fa fa-user" aria-hidden="true"></span>
@@ -55,23 +81,23 @@
                     </div>
                 </div>
                 <div class="field-group">
-                    <span class="fa fa-phone" aria-hidden="true"></span>
+                    <span class="fa fa-lock" aria-hidden="true"></span>
                     <div class="wthree-field">
-                        <input name="number" id="number" type="text" value="" placeholder="Contact Number" required>
+                        <input name="number" id="number" type="Password" value="" placeholder="password" required>
                     </div>
                 </div>
                 <div class="field-group">
                     <span class="fa fa-lock" aria-hidden="true"></span>
                     <div class="wthree-field">
-                        <input name="password" id="myInput" type="Password" placeholder="Password">
+                        <input name="password" id="myInput" type="text" placeholder="Confirm Password">
                     </div>
                 </div>
                 <div class="wthree-field">
                     <input id="saveForm" name="saveForm" type="submit" value="Register" />
                 </div>
-				<div class="account">
-					<p class="text-center">Already have an account ? <a href="#">Login</a></p>
-				</div>
+                <div class="account">
+                    <p class="text-center">Already have an account ? <a href="#">Login</a></p>
+                </div>
             </form>
         </div>
     </div>
@@ -83,3 +109,4 @@
 </body>
 <!-- //Body -->
 </html>
+
